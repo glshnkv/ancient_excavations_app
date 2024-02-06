2310abdf073151972d4bfe8d81882d121340bfed
Glushenkov <romaglushenkov02@gmail.com>
2024-02-07 00:42:39 +0400

    added loading and onboarding screens

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,3 +1,6 @@
+import 'package:ancient_excavations_app/screens/loading/loading_screen.dart';
+import 'package:ancient_excavations_app/screens/lobby/lobby_screen.dart';
+import 'package:ancient_excavations_app/screens/onboarding/onboarding_screen.dart';
 import 'package:auto_route/auto_route.dart';
 
 part 'router.gr.dart';
@@ -7,6 +10,9 @@ class AppRouter extends _$AppRouter {
 
   @override
   List<AutoRoute> get routes => [
+    AutoRoute(page: LoadingRoute.page, initial: true),
+    AutoRoute(page: OnboardingRoute.page),
+    AutoRoute(page: LobbyRoute.page),
 
   ];
 }
---
+++ lib/screens/lobby/lobby_screen.dart
@@ -0,0 +1,17 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class LobbyScreen extends StatefulWidget {
+  const LobbyScreen({super.key});
+
+  @override
+  State<LobbyScreen> createState() => _LobbyScreenState();
+}
+
+class _LobbyScreenState extends State<LobbyScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
